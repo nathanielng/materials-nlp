4172bd5569a246895f1c1001420404ade99860f9
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-04-21 21:03:51 +0800

    Add start command handler

--- bot-src/telegram-bot.py
+++ bot-src/telegram-bot.py
@@ -16,8 +16,15 @@ TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
+def start(update, ctx):
+    ctx.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Bot is ready"
+    )
+
+
 if __name__ == "__main__":
-    updater = telegram.ext.Updater(
-        token=TELEGRAM_API_TOKEN,
-        use_context=True)
+    updater = telegram.ext.Updater(token=TELEGRAM_API_TOKEN, use_context=True)
+    h_start = telegram.ext.CommandHandler('start', start)
+    updater.dispatcher.add_handler(h_start)
     updater.start_polling()
